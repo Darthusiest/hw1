160a3f7db85c1ca475484ac0ce7bc1e1fbdbe5f9
Jake Pajouhesh <pajouhes@usc.edu>
2025-09-11 01:35:35 +0000

    Implemented push_back/push_front/pop_back/pop_front/front/back getValAtLoc() with block traversal

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,21 @@
 
 int main(int argc, char* argv[])
 {
+ULListStr* unrolled_LL = new ULListStr();
 
+unrolled_LL->push_back("1");
+unrolled_LL->push_front("7");
+unrolled_LL->push_back("4");
+
+std::cout << unrolled_LL->get(0) << ", " << unrolled_LL->get(1) << ", " << unrolled_LL->get(2) << std::endl;
+std::cout << "Size: " << unrolled_LL->size() << std::endl;
+
+unrolled_LL->pop_front();
+unrolled_LL->pop_back();
+
+std::cout << unrolled_LL->get(0) << std::endl;
+
+delete unrolled_LL;
+
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,180 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+
+//Function implementation
+
+void ULListStr::push_back(const std::string& val){
+  if(head_ == nullptr){ //case 1 its empty
+    Item* newItem = new Item;
+
+    newItem->first = 0;
+    newItem->last = 0;
+
+    head_ = tail_ = newItem;
+
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+
+    size_++;
+
+  } else if(tail_->last < ARRSIZE){ //case 2 tail has room in the back
+    tail_->val[tail_->last] = val;
+
+    tail_->last++;
+
+    size_++;
+
+  } else{ //case 3 tail is full and need to allocate a new node
+    Item* newItem = new Item;
+    
+    newItem->first = 0;
+    newItem->last = 0;
+
+    newItem->prev = tail_;
+    tail_->next = newItem;
+    newItem->next = nullptr;
+    tail_ = newItem;
+
+    tail_->val[0] = val;
+    tail_->last++;
+
+    size_++;
+  }
+
+}
+
+
+void ULListStr::pop_back(){
+
+  if (head_ == nullptr) return;
+
+  if(tail_->last - tail_->first > 1){
+    tail_->last--;
+  
+  }else{
+    Item* old_tail = tail_;
+    tail_ = tail_->prev;
+    
+    if(tail_){
+      tail_->next = nullptr;
+    }else{
+      head_ = nullptr;
+      
+    }
+    delete old_tail;
+  }
+
+  size_--;
+}
+
+
+void ULListStr::push_front(const std::string& val){
+  if(head_ == nullptr){ 
+    Item* newItem = new Item;
+
+    newItem->first = 0;
+    newItem->last = 0;
+
+    head_ = tail_ = newItem;
+
+    head_->val[head_->first] = val;
+    head_->last++;
+
+    size_++;
+
+  }else if(head_->first > 0){
+    
+    head_->first--;
+    head_->val[head_->first] = val;
+    
+    
+    size_++;
+  }else{
+    Item* newItem = new Item;
+
+    newItem->first = ARRSIZE - 1;
+    newItem->last = ARRSIZE;
+
+    newItem->val[newItem->first] = val;
+
+    newItem->next = head_; 
+    head_->prev = newItem;
+    newItem->prev = nullptr;
+    head_ = newItem; 
+    size_++;
+  }
+
+}
+
+
+
+void ULListStr::pop_front(){
+  if (!head_) return;
+
+  if (head_->last - head_->first > 1){
+    head_->first++;
+  
+  }else{
+    Item* old_head = head_;
+    head_ = head_->next;
+
+    if(head_){
+      head_->prev = nullptr;
+    }else{
+      tail_ = nullptr;
+    }
+    delete old_head;
+  }
+  size_--;
+}
+
+
+std::string const& ULListStr::back() const{
+
+  if (size_ == 0) throw std::invalid_argument("empty list");
+
+  return tail_->val[tail_->last - 1];
+}
+
+
+std::string const & ULListStr::front() const{
+  if (size_ == 0) throw std::invalid_argument("empty list");
+
+  return head_->val[head_->first];
+}
+
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  //returns pointer to the i-th element 
+  //if location doesnt exit return nullptr
+  //pointer to the element will be loc 
+  //O(1) front & end
+  //O(n) walk through list, use while loop
+  
+  //case 1 empty 
+
+  //case 2 in this block 
+
+  //case 3 not there, go next node
+
+  if(loc >= size_) return nullptr; 
+
+  Item* cur = head_;
+  size_t index = loc;
+
+  while(cur){
+    size_t items_in_block = cur->last - cur->first;
+
+    if(index < items_in_block){ //item could be in block
+      return &cur->val[cur->first + index];
+    }
+
+    index -= items_in_block;
+
+    cur = cur->next;
+  }
+
+  return nullptr;
+}
